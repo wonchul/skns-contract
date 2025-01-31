f414abeea992f842cc85d2673236c99751ed4f3c
wonchul.no <wonchul.no@navercorp.com>
2025-01-31 20:40:30 +0900

    adawdwad

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,33 +32,36 @@ def resume():
     st.title(f"SKNs {contract_name} 작성")
     st.info(f"아래 {contract_name}를 검토후 내용을 입력하고 작성완료를 눌러주세요.")
     
-    if contract_name == '근로계약서':
-        bg_image_name = 'snapshot.png'
-        name_position = (350, 1220)
-        signature_position = (470, 1200)
-        year_position = (350, 1250)
-        month_position = (450, 1250)
-        day_position = (550, 1250)
-    elif contract_name == '위탁계약서':
-        bg_image_name_1 = 'contract1-1.png'
-        bg_image_name_2 = 'contract1-2.png'
+    if config["day_worker"]:  # 일용직 계약서
+        bg_image_name = 'contract1.png'
+        name_position = (1020, 2088) # 2030 - 854
+        name_position_2 = (965, 320)
 
-        name_position = (350, 1172) # 2030 - 854
-        name_position_2 = (940, 364)
-        signature_position = (470, 1146) # 2000 - 854
-        year_position = (750, 425) # 1275
-        month_position = (880, 425)
-        day_position = (990, 425)
-        addr_position = (350, 1048) # 1900 - 854
-        jumin_position = (350, 1091) # 1950 - 854
-        contact_position = (350, 1131) # 1990 - 854
+        signature_position = (1170, 2078) # 2000 - 854
+        year_position = (750, 1796) # 1275
+        month_position = (880, 1796)
+        day_position = (990, 1796)
+        addr_position = (1020, 1961) # 1900 - 854
+        jumin_position = (1020, 2003) # 1950 - 854
+        contact_position = (1020, 2043) # 1990 - 854
 
-    else:
-        st.stop()
+    else: # 중단기 계약서
+        bg_image_name = 'contract2.png'
+
+        name_position = (1020, 2070) # 2030 - 854
+        name_position_2 = (965, 320)
+        signature_position = (1170, 2060) # 2000 - 854
+        year_position = (750, 1778) # 1275
+        month_position = (880, 1778)
+        day_position = (990, 1778)
+        addr_position = (1020, 1943) # 1900 - 854
+        jumin_position = (1020, 1985) # 1950 - 854
+        contact_position = (1020, 2025) # 1990 - 854
+        date_position = (440, 490) # 2030 - 854
 
     # 배경 이미지 로드
-    background_image_1 = Image.open(bg_image_name_1).convert("RGBA")
-    background_image_2 = Image.open(bg_image_name_2).convert("RGBA")
+    background_image = Image.open(bg_image_name).convert("RGBA")
+    # background_image_2 = Image.open(bg_image_name_2).convert("RGBA")
 
 
     with st.form(key="my_form_1"):
@@ -66,17 +69,16 @@ def resume():
         # **배경 이미지 미리 보기**
         st.write(f"**{contract_name} 미리 보기:**")
         # st.image(background_image, caption="배경 이미지", use_column_width=True)
-        st.image(background_image_1, use_column_width=True)
-        st.image(background_image_2, use_column_width=True)
+        st.image(background_image, use_column_width=True)
+        # st.image(background_image_2, use_column_width=True)
 
         # 이름 입력
         # st.write("**이름을 입력하세요:**")
+        if config["day_worker"] is False:
+            st.write(f"계약기간 : {config['date_range']}")
         addr_input = st.text_input("주소 입력", placeholder="주소을 입력하세요")
-
         jumin_input = st.text_input("주민번호 입력", placeholder="주민번호를 입력하세요 (987654-1111111)")
-
         contact_input = st.text_input("연락처 입력", placeholder="연락처를 입력하세요 (010-9999-9999)")
-
         name_input = st.text_input("이름 입력", placeholder="이름을 입력하세요")
 
         # 서명 드로우 캔버스 설정
@@ -118,18 +120,17 @@ def resume():
 
                                 # 서명 이미지를 배경 이미지에 합성 (크기를 조정하여 삽입)
                                 resized_signature = signature_image.resize((200, 75))  # 서명 이미지를 200x75로 축소
-                                combined_image = background_image_2.copy()
+                                combined_image = background_image.copy()
                                 combined_image.paste(resized_signature, signature_position, resized_signature)
 
                                 # 이름 텍스트를 이미지로 변환하여 추가
                                 # if name_input:
                                 draw = ImageDraw.Draw(combined_image)
-                                draw_background = ImageDraw.Draw(background_image_1)
 
                                 # text_position = (x_position_name, y_position_name)  # 이름 위치 (조정)
                                 # 폰트 로딩
                                 try:
-                                    font = ImageFont.truetype("./fonts/NanumGothic-Regular.ttf", size=25)
+                                    font = ImageFont.truetype("./fonts/NanumGothic-Regular.ttf", size=26)
                                 except IOError:
                                     font = ImageFont.load_default()  # 폰트 파일을 찾을 수 없으면 기본 폰트 사용
 
@@ -145,7 +146,9 @@ def resume():
                                 draw.text(month_position, f"{month}", fill="black", font=font)
                                 draw.text(day_position, f"{day}", fill="black", font=font)
 
-                                draw_background.text(name_position_2, name_input, fill="black", font=font)
+                                draw.text(name_position_2, name_input, fill="black", font=font)
+                                if config["day_worker"] is False:
+                                    draw.text(date_position, config['date_range'], fill="black", font=font)
 
 
                                 # # 이미지를 저장할 수 있는 버퍼 생성
@@ -153,19 +156,11 @@ def resume():
                                 # combined_image.save(buffer, format="PNG")
                                 # buffer.seek(0)
                                 # st.success("이미지가 성공적으로 저장되었습니다.")
-                                buffer1 = io.BytesIO()
-                                buffer2 = io.BytesIO()
+                                buffer = io.BytesIO()
                                 # Save the images to the buffers
-                                combined_image.save(buffer1, format="PNG")
-                                background_image_1.save(buffer2, format="PNG")                  
-                                buffer1.seek(0)
-                                buffer2.seek(0)
-                                zip_buffer = io.BytesIO()
-                                with zipfile.ZipFile(zip_buffer, "a", zipfile.ZIP_DEFLATED) as zip_file:
-                                    zip_file.writestr('signed_with_name_image1.png', buffer2.getvalue())
-                                    zip_file.writestr('signed_with_name_image2.png', buffer1.getvalue())
+                                combined_image.save(buffer, format="PNG")
 
-                                zip_buffer.seek(0)                                
+                                buffer.seek(0)
                                 
                                 sender_email = config['sender']
                                 receiver_email = config['receiver']
@@ -188,7 +183,6 @@ def resume():
                                 <html>
                                 <body>
                                     <p>{body}</p>
-                                    <img src="cid:image2" width="800" height="1080">
                                     <img src="cid:image1" width="800" height="1080">
                                 </body>
                                 </html>
@@ -196,27 +190,17 @@ def resume():
                                 msg_alternative.attach(msg_html)
 
                                 # Attach first image
-                                msg_image1 = MIMEImage(buffer1.getvalue(), _subtype="png")
-                                msg_image1.add_header('Content-ID', '<image1>')
-                                msg.attach(msg_image1)
-
-                                # Attach second image
-                                msg_image2 = MIMEImage(buffer2.getvalue(), _subtype="png")
-                                msg_image2.add_header('Content-ID', '<image2>')
-                                msg.attach(msg_image2)
+                                msg_image = MIMEImage(buffer.getvalue(), _subtype="png")
+                                msg_image.add_header('Content-ID', '<image1>')
+                                msg.attach(msg_image)
 
                                 # Attach images as files
-                                part1 = MIMEBase('application', 'octet-stream')
-                                part1.set_payload(buffer1.getvalue())
-                                encoders.encode_base64(part1)
-                                part1.add_header('Content-Disposition', "attachment; filename= signed_with_name_image1.png")
-                                msg.attach(part1)
+                                part = MIMEBase('application', 'octet-stream')
+                                part.set_payload(buffer.getvalue())
+                                encoders.encode_base64(part)
+                                part.add_header('Content-Disposition', "attachment; filename= signed_with_name.png")
+                                msg.attach(part)
 
-                                part2 = MIMEBase('application', 'octet-stream')
-                                part2.set_payload(buffer2.getvalue())
-                                encoders.encode_base64(part2)
-                                part2.add_header('Content-Disposition', "attachment; filename= signed_with_name_image2.png")
-                                msg.attach(part2)
 
                                 with st.spinner(f'{contract_name} 생성중 조금만 기다려주세요...'):
                                     try:
@@ -230,18 +214,11 @@ def resume():
                                     except Exception as e:
                                         st.error(f"작성 실패했습니다 담당자에게 문의해주세요.")
 
-                                # 최종 이미지 다운로드 버튼 제공
-                                # st.download_button(
-                                #     label=f"{contract_name} 다운로드",
-                                #     data=buffer,
-                                #     file_name="signed_with_name_image.png",
-                                #     mime="image/png",
-                                # )
                                 st.download_button(
                                     label=f"{contract_name} 다운로드",
-                                    data=zip_buffer,
-                                    file_name=f"skns_contract_signed_{today}.zip",
-                                    mime="application/zip",
+                                    data=buffer,
+                                    file_name=f"skns_contract_signed_{today}.png",
+                                    mime="image/png",
                                 )                                
                         else:
                             st.error("서명을 작성하고 작성완료 버튼을 눌러주세요.")
